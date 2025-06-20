cb23ea36ce48d96b05d5d67cb5d6d32e6f4b1721
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-06-20 21:25:47 +0530

    refactor: update the gloval routing confugration
    - update ProtectedRoute to use authentication

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -3,28 +3,32 @@ import Projects from "../pages/Projects";
 import MainLayout from "../layouts/MainLayout";
 import Podcasts from "../pages/Podcasts";
 import EditPodcasts from "../pages/EditPodcast";
-import EditProfile from "../pages/EditProfile";
 import ProtectedRoute from "./ProtectedRoute";
-import LoginPage from "../pages/LoginPage";
+import AuthLayout from "../layouts/AuthLayout";
+import { LoginForm, RegisterForm } from "@/components/auth/AuthForms";
+import NotFound from "@/pages/NotFound";
+import AccountSettings from "@/components/auth/AccountSettings";
 
 const AppRoutes = () => (
   <Routes>
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<LoginPage />} />
+    <Route path="/" element={<AuthLayout />}>
+      <Route path="login" element={<LoginForm />} />
+      <Route path="register" element={<RegisterForm />} />
+    </Route>
+    <Route path="projects" element={<Projects />} />
 
     <Route
-      path="/"
+      path="/:projectId"
       element={
         <ProtectedRoute>
           <MainLayout />
         </ProtectedRoute>
       }
     >
-      <Route index element={<Projects />} />
-      <Route path=":projectId" element={<Podcasts />} />
-      <Route path=":projectId/:podcastId/view" element={<EditPodcasts />} />
-      <Route path=":projectId/:podcastId/edit" element={<EditPodcasts />} />
-      <Route path="user/edit" element={<EditProfile />} />
+      <Route index element={<Podcasts />} />
+      <Route path=":podcastId" element={<EditPodcasts />} />
+      <Route path="user/edit" element={<AccountSettings />} />
+      <Route path="404/*" element={<NotFound />} />
     </Route>
   </Routes>
 );
