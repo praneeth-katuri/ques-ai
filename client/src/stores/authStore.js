2a6d3378a07190d4cf1767a43cd56db2766ddebd
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-06-20 21:26:44 +0530

    feat: add a global apiLoading state
    - added a global loading state for all api calls to show a loading spinner on updates

--- client/src/stores/authStore.js
+++ client/src/stores/authStore.js
@@ -5,9 +5,11 @@ export const useAuthStore = create((set) => ({
   accessToken: null,
   isAuthenticated: false,
   isLoading: true,
+  apiLoading: false,
 
   setAccessToken: (token) => set({ accessToken: token }),
   setUser: (user) => set({ user }),
   setIsAuthenticated: (value) => set({ isAuthenticated: value }),
   setIsLoading: (value) => set({ isLoading: value }),
+  setApiLoading: (value) => set({ apiLoading: value }),
 }));
